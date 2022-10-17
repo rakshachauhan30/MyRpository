4957c5ba94401a7a095618aecd5e5d3cfe323868
rakshachauhan30 <115777174+rakshachauhan30@users.noreply.github.com>
2022-10-17 15:47:32 +0530

    Update Demo1.java

--- GitHub/src/Demo1.java
+++ GitHub/src/Demo1.java
@@ -2,7 +2,7 @@
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello its Rakshas first repo......");
-		System.out.println("************************************")
+		System.out.println("************************************");
 			public void m1()
 		{
 			
