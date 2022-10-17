2e3910e96521468e85c3d8708fa88495129ad074
rakshachauhan30 <115777174+rakshachauhan30@users.noreply.github.com>
2022-10-17 15:44:25 +0530

    Update Demo1.java
    second commit

--- GitHub/src/Demo1.java
+++ GitHub/src/Demo1.java
@@ -3,7 +3,10 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello its Rakshas first repo......");
 		System.out.println("************************************")
-		
+			public void m1()
+		{
+			
+		System.out.println("Github***********");
 	}
 
 }
